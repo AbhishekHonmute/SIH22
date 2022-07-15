0f8dbb4910eb52634ba1fe23a3ffb13a150f5b94
Versatile <honmuteabhishek@gmail.com>
2022-07-15 15:26:31 +0530

    frontend: Login page created

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Typography } from "@mui/material";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <Typography>Dashboard</Typography>;
+  }
+}
+
+export default Dashboard;
